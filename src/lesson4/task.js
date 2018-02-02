26e6dcac2100fa234e94b6c4d5272bded9d57b07
andrii <andrii.iermakov@globallogic.com>
2018-02-02 19:33:57 +0200

    + Modified task

--- src/lesson4/task.js
+++ src/lesson4/task.js
@@ -2,7 +2,7 @@
   Write a function that creates custom set object. Function
   accepts single optional parameter (array) do define initial
   set content.
-  Set should have add(), has(), delete(), forEach() methods
+  Set should have add(), has(), delete(), forEach(), clear() methods
   and size property. Should not use es6 objects Set, WeakSet,
   but work in similar way. Set should preserve addition order
   in forEach() method.
@@ -16,8 +16,8 @@ export function createSet(arr) {
   Write a function that creates custom map object. Function
   accepts single optional parameter (array) do define initial
   map content.
-  Map should have set(), get(), has(), delete(), forEach() methods
-  and size property. Should not use es6 objects Map, WeakMap,
+  Map should have set(), get(), has(), delete(), forEach(), clear()
+  methods and size property. Should not use es6 objects Map, WeakMap,
   but work in similar way. Map should preserve addition order
   in forEach() method.
   myMap = createMap([['a', 1], ['b', 2], ['c', 3]])
