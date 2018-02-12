f8ee9a26d388bfa300a21285a446308b85900bb8
vladkampov <vlad@geeny.io>
2018-02-12 16:52:14 +0200

    Add lesson6 task

---
+++ src/lesson6/task.js
@@ -0,0 +1,27 @@
+// с помощью Fetch API и swapi.co API получить следующие данные
+
+// Климат на любой планете по её имени
+// {planetName} – String
+const getClimate = function(planetName) {
+
+}
+
+// Получить информацию (Object) о любом персонаже по имени
+// {name} – String
+const getProfile = function(name) {
+
+}
+
+// Получить список пилотов (имена в виде Array of Strings) космического корабля
+// по его названию
+// {starshipName} - String
+const getPilots = function(starshipName) {
+
+}
+
+
+export default {
+  getClimate,
+  getProfile,
+  getPilots
+}
